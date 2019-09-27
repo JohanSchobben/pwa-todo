2a07444d7720c6a7872e034534eee41fcbe521db
JohanSchobben <johanschobben@gmail.com>
2019-09-27 10:22:28 +0200

    stored firebass appl as local file

--- src/sw.js
+++ src/sw.js
@@ -1,8 +1,36 @@
-const LOG_PREFIX = "[Service worker] ";
+const CACHE_NAME = "todopwa";
+const CACHED_FILES = [
+  "/",
+  "index.html",
+  "/static/config.json",
+  "/js/builder.js",
+  "/js/firebase.js",
+  "/js/helper.js",
+  "/js/main.js",
+  "/img/plus.svg",
+  "https://code.jquery.com/jquery-3.3.1.slim.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
+  "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",
+  "https://www.gstatic.com/firebasejs/6.6.2/firebase-app.js",
+  "https://www.gstatic.com/firebasejs/6.6.2/firebase-database.js",
+  "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+];
+
 self.addEventListener("install", function (event) {
-  console.log(LOG_PREFIX + "is installed!", event);
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function (cache) {
+          return cache.addAll(CACHED_FILES);
+        })
+    );
 });
 
 self.addEventListener("activate", function (event) {
   console.log(LOG_PREFIX + "is activated!", event);
+});
+
+self.addEventListener("fetch", function (event) {
+  caches.match(event.request).then(function(response) {
+    return response || fetch(event.request);
+  });
 });
