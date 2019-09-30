92072d241bb433d86b2bf42d41482b112fbd44c9
JohanSchobben <johanschobben@gmail.com>
2019-09-30 16:12:33 +0200

    now saving when fetching from the internet

--- src/sw.js
+++ src/sw.js
@@ -8,11 +8,11 @@ const CACHED_FILES = [
   "/js/helper.js",
   "/js/main.js",
   "/img/plus.svg",
+  "/js/firebase-app.js",
+  "/js/firebase-database.js",
   "https://code.jquery.com/jquery-3.3.1.slim.min.js",
   "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
   "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",
-  "https://www.gstatic.com/firebasejs/6.6.2/firebase-app.js",
-  "https://www.gstatic.com/firebasejs/6.6.2/firebase-database.js",
   "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
 ];
 
@@ -25,12 +25,17 @@ self.addEventListener("install", function (event) {
     );
 });
 
-self.addEventListener("activate", function (event) {
-  console.log(LOG_PREFIX + "is activated!", event);
-});
-
 self.addEventListener("fetch", function (event) {
-  caches.match(event.request).then(function(response) {
-    return response || fetch(event.request);
-  });
+  const fetchResponse = caches.match(event.request)
+    .then(function(result){
+      if (result) return result;
+      return fetch(event.request)
+        .then(function (response) {
+          return caches.open(CACHE_NAME)
+            .then(function (cache) {
+              cache.put(event.request.url, response.clone())
+            });
+        });
+    });
+  event.respondWith(fetchResponse);
 });
