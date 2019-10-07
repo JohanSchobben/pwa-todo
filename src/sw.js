25e1930fe7b256db65b37e183e9349c6fd9039a0
JohanSchobben <johanschobben@gmail.com>
2019-10-07 15:58:26 +0200

    updated service worker to not cache any of the deleted files

--- src/sw.js
+++ src/sw.js
@@ -1,17 +1,14 @@
-const VERSION = 9;
+const VERSION = 10;
 const CACHE_NAME = "todopwa-cache";
 const CACHED_FILES = [
   "/",
   "index.html",
   "/static/config.json",
   "/js/builder.js",
-  "/js/firebase.js",
   "/js/helper.js",
   "/js/network.js",
   "/js/main.js",
   "/img/plus.svg",
-  "/js/firebase-app.js",
-  "/js/firebase-database.js",
   "https://code.jquery.com/jquery-3.3.1.slim.min.js",
   "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
   "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",
